55ceffa5dd2b19e1adc4e280473f215e03e56124
Giovanni Bassi <giggio@giggio.net>
2020-06-15 18:58:05 -0300

    Small improvements in git handling

--- install.sh
+++ install.sh
@@ -17,8 +17,4 @@ git submodule update --init --recursive
 popd > /dev/null
 
 echo -e "\e[34mWorking on unpriviledged setup.\e[0m"
-"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "install.conf.yaml" "${@}"
-
-# echo -e "\e[34mWorking on root setup.\e[0m"
-# sudo "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" --plugin "$BASEDIR/dotbot_plugin_aptget/aptget.py" -d "${BASEDIR}" -c "install-pkgs.conf.yaml" "${@}"
-# sudo "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" --plugin "$BASEDIR/dotbot-apt-get/aptget.py" -d "${BASEDIR}" -c "install-pkgs.conf.yaml" "${@}"
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "$BASEDIR/install.conf.yaml" "${@}"

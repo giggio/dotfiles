deb0551ef203fad2a9ede903c9a8ded3713117ca
Giovanni Bassi <giggio@giggio.net>
2023-11-04 12:35:00 -0300

    Refactor systemd scripts

--- configure-systemd.sh
+++ configure-systemd.sh
@@ -49,40 +49,61 @@ EOF
 fi
 
 if $VERBOSE; then
-  writeGreen "Running `basename "$0"` $ALL_ARGS"
-  Update is $UPDATE
+  writeGreen "Running `basename "$0"` $ALL_ARGS
+  Update is $UPDATE"
 fi
 
-NEEDS_RELOAD=false
+function create_systemd_service_and_timer {
+  local NEEDS_RELOAD=false
+  if ! [ -v 1 ]; then
+    die "No service name provided."
+  fi
+  local SERVICE=$1
+  if [[ $SERVICE = *" "* ]]; then
+    die "Service name cannot contain spaces."
+  fi
+  local SOURCE_SERVICE_FILE="$BASEDIR"/systemd/$SERVICE.service
+  if ! [ -f "$SOURCE_SERVICE_FILE" ]; then
+    die "Service file $SOURCE_SERVICE_FILE does not exist."
+  fi
+  local DESTINATION_SERVICE_FILE=/etc/systemd/system/$SERVICE.service
+  # shellcheck disable=SC2086
+  if ! [ -f $DESTINATION_SERVICE_FILE ] || ! cmp --quiet "$SOURCE_SERVICE_FILE" $DESTINATION_SERVICE_FILE; then
+    cp "$SOURCE_SERVICE_FILE" $DESTINATION_SERVICE_FILE
+    NEEDS_RELOAD=true
+  elif $VERBOSE; then
+    writeBlue "No need to copy $SOURCE_SERVICE_FILE to $DESTINATION_SERVICE_FILE, it already exists and is the same."
+  fi
 
-SERVICE=wsl-clean-memory
-SOURCE_SERVICE_FILE="$BASEDIR"/systemd/$SERVICE.service
-DESTINATION_SERVICE_FILE=/etc/systemd/system/$SERVICE.service
-if ! [ -f $DESTINATION_SERVICE_FILE ] || ! cmp --quiet "$SOURCE_SERVICE_FILE" $DESTINATION_SERVICE_FILE; then
-  cp "$SOURCE_SERVICE_FILE" $DESTINATION_SERVICE_FILE
-  NEEDS_RELOAD=true
-else
-  writeBlue "No need to copy $SOURCE_SERVICE_FILE to $DESTINATION_SERVICE_FILE, it already exists and is the same."
-fi
+  local SOURCE_TIMER_FILE="$BASEDIR"/systemd/$SERVICE.timer
+  if [ -f "$SOURCE_TIMER_FILE" ]; then
+    local DESTINATION_TIMER_FILE=/etc/systemd/system/$SERVICE.timer
+    # shellcheck disable=SC2086
+    if ! [ -f $DESTINATION_TIMER_FILE ] || ! cmp --quiet "$SOURCE_TIMER_FILE" $DESTINATION_TIMER_FILE; then
+      cp "$SOURCE_TIMER_FILE" $DESTINATION_TIMER_FILE
+      NEEDS_RELOAD=true
+    elif $VERBOSE; then
+      writeBlue "No need to copy $SOURCE_TIMER_FILE to $DESTINATION_TIMER_FILE, it already exists and is the same."
+    fi
+  fi
 
-SOURCE_TIMER_FILE="$BASEDIR"/systemd/$SERVICE.timer
-DESTINATION_TIMER_FILE=/etc/systemd/system/$SERVICE.timer
-if ! [ -f $DESTINATION_TIMER_FILE ] || ! cmp --quiet "$SOURCE_TIMER_FILE" $DESTINATION_TIMER_FILE; then
-  cp "$SOURCE_TIMER_FILE" $DESTINATION_TIMER_FILE
-  NEEDS_RELOAD=true
-else
-  writeBlue "No need to copy $SOURCE_TIMER_FILE to $DESTINATION_TIMER_FILE, it already exists and is the same."
-fi
+  local SOURCE_SCRIPT_FILE="$BASEDIR"/systemd/$SERVICE
+  if [ -f "$SOURCE_SCRIPT_FILE" ]; then
+    local DESTINATION_SCRIPT_FILE=/usr/local/sbin/$SERVICE
+    # shellcheck disable=SC2086
+    if ! [ -f $DESTINATION_SCRIPT_FILE ] || ! cmp --quiet "$SOURCE_SCRIPT_FILE" $DESTINATION_SCRIPT_FILE; then
+      cp "$SOURCE_SCRIPT_FILE" $DESTINATION_SCRIPT_FILE
+      NEEDS_RELOAD=true
+    elif $VERBOSE; then
+      writeBlue "No need to copy $SOURCE_SCRIPT_FILE to $DESTINATION_SCRIPT_FILE, it already exists and is the same."
+    fi
+  fi
 
-SOURCE_SCRIPT_FILE="$BASEDIR"/systemd/$SERVICE
-DESTINATION_SCRIPT_FILE=/usr/local/sbin/$SERVICE
-if ! [ -f $DESTINATION_SCRIPT_FILE ] || ! cmp --quiet "$SOURCE_SCRIPT_FILE" $DESTINATION_SCRIPT_FILE; then
-  cp "$SOURCE_SCRIPT_FILE" $DESTINATION_SCRIPT_FILE
-  NEEDS_RELOAD=true
-else
-  writeBlue "No need to copy $SOURCE_SCRIPT_FILE to $DESTINATION_SCRIPT_FILE, it already exists and is the same."
-fi
+  if $NEEDS_RELOAD; then
+    systemctl daemon-reload
+    # shellcheck disable=SC2086
+    systemctl enable $SERVICE.timer
+  fi
+}
 
-if $NEEDS_RELOAD; then
-  systemctl daemon-reload
-fi
+create_systemd_service_and_timer wsl-clean-memory

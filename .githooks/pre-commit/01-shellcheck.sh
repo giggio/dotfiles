f64f1bd28efb4e9ed72835e527dcd5fa7672abb7
Giovanni Bassi <giggio@giggio.net>
2024-01-23 20:22:12 -0300

    Update pre-commit git hook and shellcheck

--- .githooks/pre-commit/01-shellcheck.sh
+++ .githooks/pre-commit/01-shellcheck.sh
@@ -2,10 +2,15 @@
 
 set -euo pipefail
 
-submodule_dirs=$(git submodule | awk '{ printf "-not -path '"`pwd`"'/" $2 "/* " }')
+DIR="$(pwd)" # hooks are always ran from the root of the repo
+
+submodule_dirs=$(git submodule | awk '{ printf "-not -path '"$DIR"'/" $2 "/* " }')
 set -f # disable globbing
 # shellcheck disable=SC2086
-files=$(find "$(pwd)" -name '*.sh' $submodule_dirs)
+sh_files=$(find "$DIR" \( -name '*.sh' -or -name '*.bash' \) -not -path "$DIR"/.testsupport/'*' $submodule_dirs)
 # shellcheck disable=SC2086
-shellcheck --shell bash $files
+exec_files=$(find "$DIR" -not -path "$DIR"/.testsupport/'*' $submodule_dirs -type f -exec sh -c 'head -n1 $1 | grep -qE '"'"'^#!(.*/|\/usr\/bin\/env +)bash'"'" sh {} \; -exec echo {} \;)
 set +f
+all_files=`echo -e "$sh_files\n$exec_files" | sort | uniq`
+# shellcheck disable=SC2086
+shellcheck --shell bash $all_files
--- run_shellcheck.sh
+++ run_shellcheck.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-EXEC_FILES=`find "$DIR" ! \( -path "$DIR"/.git -prune \) ! \( -path "$DIR"/vimfiles -prune \) ! \( -path "$DIR"/tools -prune \) ! \( -path "$DIR"/bashscripts -prune \) ! \( -path "$DIR"/poshfiles -prune \) ! \( -path "$DIR"/nvim -prune \) ! \( -path "$DIR"/nuscripts -prune \) ! \( -path "$DIR"/dotbot -prune \) ! \( -path "$DIR"/.testsupport -prune \) -type f -exec sh -c 'head -n1 $1 | grep -qE '"'"'^#!(.*/|\/usr\/bin\/env +)bash'"'" sh {} \; -exec echo {} \;`
-SH_FILES=`find "$DIR" ! \( -path "$DIR"/.git -prune \) ! \( -path "$DIR"/vimfiles -prune \) ! \( -path "$DIR"/tools -prune \) ! \( -path "$DIR"/bashscripts -prune \) ! \( -path "$DIR"/poshfiles -prune \) ! \( -path "$DIR"/nvim -prune \) ! \( -path "$DIR"/nuscripts -prune \) ! \( -path "$DIR"/dotbot -prune \) ! \( -path "$DIR"/.testsupport -prune \) -type f -name '*.sh'`
-ALL_FILES=`echo -e "$EXEC_FILES\n$SH_FILES" | sort | uniq`
-echo -e "Checking files:\n$ALL_FILES"
+
+submodule_dirs=$(git submodule | awk '{ printf "-not -path '"$DIR"'/" $2 "/* " }')
+set -f # disable globbing
+# shellcheck disable=SC2086
+sh_files=$(find "$DIR" \( -name '*.sh' -or -name '*.bash' \) -not -path "$DIR"/.testsupport/'*' $submodule_dirs)
+# shellcheck disable=SC2086
+exec_files=$(find "$DIR" -not -path "$DIR"/.testsupport/'*' $submodule_dirs -type f -exec sh -c 'head -n1 $1 | grep -qE '"'"'^#!(.*/|\/usr\/bin\/env +)bash'"'" sh {} \; -exec echo {} \;)
+set +f
+all_files=`echo -e "$sh_files\n$exec_files" | sort | uniq`
 pushd "$DIR" > /dev/null
 # shellcheck disable=SC2086
-shellcheck $ALL_FILES
+shellcheck --shell bash $all_files
 popd > /dev/null

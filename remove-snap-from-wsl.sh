0040f6806766f94e4d0d9045b7399cd3898f1546
Giovanni Bassi <giggio@giggio.net>
2024-04-29 23:03:20 -0300

    Add script to remove snap from wsl

---
+++ remove-snap-from-wsl.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$BASEDIR"/_common-setup.sh
+
+if [ "$EUID" != "0" ]; then
+  die "Please run this script as root"
+fi
+
+SHOW_HELP=false
+VERBOSE=false
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --help|-h)
+    SHOW_HELP=true
+    break
+    ;;
+    --verbose)
+    VERBOSE=true
+    shift
+    ;;
+    *)
+    shift
+    ;;
+  esac
+done
+eval set -- "$PARSED_ARGS"
+
+if $SHOW_HELP; then
+  cat <<EOF
+Installs the dotfiles.
+
+Usage:
+  `readlink -f "$0"` [flags]
+
+Flags:
+      --verbose            Show verbose output
+  -h, --help               help
+EOF
+  exit 0
+fi
+
+if $VERBOSE; then
+  writeGreen "Running `basename "$0"` $ALL_ARGS
+fi
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+snap list
+snap remove ubuntu-desktop-installer
+snap remove gtk-common-themes
+snap remove bare
+snap remove core22
+snap remove core22
+snap list
+
+systemctl disable snapd.service
+systemctl disable snapd.socket
+systemctl disable snapd.seeded.service
+
+rm -rf /var/cache/snapd/
+
+apt autoremove --purge snapd
+
+rm -rf ~/snap
+rm -rf /home/$SUDO_USER/snap
+
+cat <<EOF > /etc/apt/preferences.d/nosnap.pref
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+EOF
+apt-get update
+

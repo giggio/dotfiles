450e3877e865b61df0d4f73ca731be834f4cc067
Giovanni Bassi <giggio@giggio.net>
2024-06-02 21:50:17 -0300

    Update hm state version to 24.05

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -43,14 +43,7 @@ rec {
   home = {
     username = "giggio";
     homeDirectory = "/home/" + home.username;
-
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    stateVersion = "23.11"; # Please read the comment before changing.
+    stateVersion = "24.05"; # Check if there are state version changes before changing this fiels: https://nix-community.github.io/home-manager/release-notes.xhtml
     packages =
       let
         basic_pkgs = (with pkgs; [

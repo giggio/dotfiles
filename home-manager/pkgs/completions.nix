ed4bbbeb08b570c910cb5c38673b8045cfd9c375
Giovanni Bassi <giggio@giggio.net>
2024-08-28 13:25:37 -0300

    Remove stale comment

--- home-manager/pkgs/completions.nix
+++ home-manager/pkgs/completions.nix
@@ -5,7 +5,6 @@ stdenv.mkDerivation {
   version = "0.0.1";
   src = ./completions;
   nativeBuildInputs = [ installShellFiles ];
-  # todo: remove node when https://github.com/NixOS/nixpkgs/issues/316507 is fixed
   buildPhase = ''
     ${kubectl}/bin/kubectl completion bash | sed 's/kubectl/kubecolor/g' > kubecolor.bash
     echo '_completion_loader home-manager; complete -o default -F _home-manager_completions hm' > hm.bash

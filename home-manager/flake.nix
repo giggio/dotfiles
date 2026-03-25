d34bbe0f8948cbad34d3461fb81c3ba493fe2252
Giovanni Bassi <giggio@giggio.net>
2026-03-25 00:37:27 -0300

    Change nixpkgs flake input to nixpkgs-unstable branch
    The nixos-unstable is for NixOS, and I'm only running home-manager on Ubuntu.

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of giggio";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";

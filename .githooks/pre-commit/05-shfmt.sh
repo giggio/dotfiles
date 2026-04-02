fb4ff855a8849c7721328ea5be53a5be411430d6
Giovanni Bassi <giggio@giggio.net>
2026-04-02 19:36:42 -0300

    Fix grep stray \ before / in githooks

--- .githooks/pre-commit/05-shfmt.sh
+++ .githooks/pre-commit/05-shfmt.sh
@@ -9,7 +9,7 @@ all_diff_files=$(git diff --name-only --cached --ignore-submodules=all --diff-fi
 if [ -z "$all_diff_files" ]; then exit 0; fi
 diff_files=$(echo "$all_diff_files" | grep -v /.testsupport/)
 # shellcheck disable=SC2086
-exec_files=$(find $diff_files -exec sh -c 'head -n1 $1 | grep -qE '"'"'^#!(.*/|\/usr\/bin\/env +)bash'"'" sh {} \; -exec echo {} \;)
+exec_files=$(find $diff_files -exec sh -c 'head -n1 $1 | grep -qE '"'"'^#!(.*/|/usr/bin/env +)bash'"'" sh {} \; -exec echo {} \;)
 # shellcheck disable=SC2086
 sh_files=$(find $diff_files \( -name '*.sh' -or -name '*.bash' \))
 all_files=$(echo -e "$sh_files\n$exec_files" | sort | uniq)
